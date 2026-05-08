76327927ab87c97fbb76d2ec83293803a2afd603
hyungood <43487022+hyungood@users.noreply.github.com>
2026-05-08 15:53:06 +0900

    Fix typo in markdown function name

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 st.title("☘🌳🏔2026 학교 등산 행사 안내 지도")
 st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
 st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해보세요.")
-st.markdonw("#큰 제목")
+st.markdown("#큰 제목")
 st.markdown("##작은 제목")
 st.markdown("**굵은 글씨**와 *이탤릭체*")
 
