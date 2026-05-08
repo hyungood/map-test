0615bc9065022e73fb15fe44bf5ce3e8f105ba7f
hyungood <43487022+hyungood@users.noreply.github.com>
2026-05-08 15:50:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import pandas as pd
 # 1. 웹페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
 
-st.title("🏔🏔2026 학교 등산 행사 안내 지도")
+st.title("☘🌳🏔2026 학교 등산 행사 안내 지도")
+st.markdown("우리 동아리가 직접 발로 뛰며 만든 코스 가이드 입니다.")
+st.markdown("왼쪽 메뉴에서 코스를 선택하고 행사에 참여해보세요.")
 st.text("안녕~~ 남동고 여러분. 첫 페이지를 만드셨습니다.")
